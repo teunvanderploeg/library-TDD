ef283219e50cfc552f377ba63073ba6c4977fdc9
teunvanderploeg <29253@ma-web.nl>
2021-07-16 14:46:36 +0200

    Adds author routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\AuthorController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/books', [BooksController::class, 'store']);
 Route::patch('/books/{book}', [BooksController::class, 'update']);
+Route::delete('/books/{book}', [BooksController::class, 'destroy']);
+
+Route::post('/author', [AuthorController::class, 'store']);
