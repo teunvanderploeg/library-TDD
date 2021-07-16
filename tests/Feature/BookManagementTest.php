b186518185fef2e326a65799699860f04b5e82ec
teunvanderploeg <29253@ma-web.nl>
2021-07-16 14:46:18 +0200

    Wip for books resource

--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -6,7 +6,7 @@ use App\Models\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class BookReservationTest extends TestCase
+class BookManagementTest extends TestCase
 {
     use RefreshDatabase;
 
@@ -21,8 +21,10 @@ class BookReservationTest extends TestCase
             ]
         );
 
-        $response->assertOk();
+        $book = Book::first();
+
         $this->assertCount(1, Book::all());
+        $response->assertRedirect($book->path());
     }
 
     /** @test */
@@ -56,7 +58,6 @@ class BookReservationTest extends TestCase
     /** @test */
     public function a_book_can_be_updated()
     {
-        $this->withoutExceptionHandling();
         $this->post(
             '/books',
             [
@@ -67,7 +68,7 @@ class BookReservationTest extends TestCase
 
         $book = Book::first();
         $response = $this->patch(
-            '/books/' . $book->id,
+            $book->path(),
             [
                 'title' => 'New Book Title',
                 'author' => 'New author',
@@ -76,5 +77,27 @@ class BookReservationTest extends TestCase
 
         $this->assertEquals('New Book Title', Book::first()->title);
         $this->assertEquals('New author', Book::first()->author);
+        $response->assertRedirect($book->fresh()->path());
+    }
+
+    /** @test */
+    public function a_book_can_be_deleted()
+    {
+        $this->withoutExceptionHandling();
+        $this->post(
+            '/books',
+            [
+                'title' => 'Book Title',
+                'author' => 'Teun',
+            ]
+        );
+
+        $book = Book::first();
+        $this->assertCount(1, Book::all());
+
+        $response = $this->delete($book->path());
+
+        $this->assertCount(0, Book::all());
+        $response->assertRedirect('/books');
     }
 }
