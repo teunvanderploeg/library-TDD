52d76cdc3ef4075e8400cda18b69b9f77938115a
teunvanderploeg <29253@ma-web.nl>
2021-07-28 14:16:05 +0200

    Added functions to the book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Book extends Model
 {
@@ -16,6 +15,36 @@ class Book extends Model
     {
         return '/books/' . $this->id;
     }
+
+    public function checkout($user)
+    {
+        $this->reservations()->create(
+            [
+                'user_id' => $user->id,
+                'checked_out_at' => now(),
+            ]
+        );
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()
+            ->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if(is_null($reservation)){
+            throw new \Exception();
+        }
+
+        $reservation->update(
+            [
+                'checked_in_at' => now()
+            ]
+        );
+    }
+
     public function setAuthorIdAttribute($author)
     {
         $this->attributes['author_id'] = (Author::firstOrCreate(
@@ -23,5 +52,10 @@ class Book extends Model
                 'name' => $author,
             ]
         ))->id;
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
